ce5a88597960674e647d52ddbc76361bcd8d1885
Eyvind <eyvind.malde@gmail.com>
2024-08-20 11:17:26 +0200

    pass extension 2 - receipt

--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using exercise.main;
+
 Console.WriteLine("Hello, World!");
+
+Basket b = new Basket();
+BagelShop.ChangeCapacity("manager", 25);
+
+b.AddItem("bagel", "onion");
+b.AddItem("coffee", "black");
+b.AddItem("bagel", "onion");
+b.AddItem("coffee", "lattee");
+b.AddItem("filling", "bacon");
+
+Receipt r = new Receipt(b);
+
+r.PrintReceipt();
--- exercise.main/Receipt.cs
+++ exercise.main/Receipt.cs
@@ -11,6 +11,7 @@ namespace exercise.main
         private DateTime _receiptDate;
         private string _shopName;
         private decimal _totalPrice;
+        private List<Item> _items;
         private Dictionary<string, decimal> _discounts;
         private string _receipt;
 
@@ -18,16 +19,42 @@ namespace exercise.main
         {
             _receiptDate = DateTime.Now;
             _shopName = BagelShop.Name;
+            _items = basket.Items;
             _totalPrice = basket.GetTotalCost();
             _discounts = basket.GetDiscounts();
             _receipt = GenerateReceipt();
         }
 
-        private string GenerateReceipt() { return string.Empty; }
+        private string GenerateReceipt()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{_shopName}\n");
+            sb.AppendLine($"{_receiptDate:yyyy-MM-dd HH:mm:ss}\n");
+
+            var groupedItems = _items
+                .GroupBy(item => new { item.Name, item.Variant })
+                .Select(group => new
+                {
+                    Name = $"{group.Key.Variant} {group.Key.Name}",
+                    Quantity = group.Count(),
+                    TotalPrice = group.Sum(item => item.Price)
+                })
+                .OrderBy(item => item.Name);
+
+            foreach (var item in groupedItems)
+            {
+                sb.AppendLine($"{item.Name} {item.Quantity} {item.TotalPrice}");
+            }
+
+            sb.AppendLine($"Total price: {_totalPrice}");
+
+            return sb.ToString();
+        }
 
         public void PrintReceipt()
         {
-
+            Console.WriteLine(_receipt);
         }
     }
 }
